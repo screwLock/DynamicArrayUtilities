0524aeab4b6f218bab85e23df2e15df59d3354f1
Travus <helmlyw@gmail.com>
2015-08-07 23:30:44 -0400

    createDynArr(),printSize(), findSize() has been refactored

--- DynamicArray.c
+++ DynamicArray.c
@@ -8,28 +8,28 @@ Output:   Returns a pointer to the array if
           creation was successful.  Returns
           -1 if an error occurred.
 
-Other Changes:  The last value is set to NULL
-                to mark it as the last entry.
+Other Changes:  None
 
 *************************************************/
 
 int *createDynArr(int size){
 
-    int *dynArr = (int*) malloc(size * sizeof(int));
+    //Create an extra block of memory.
+    //The first block will be a hidden block
+    //that holds the size of the array.
+    int sizeHolder = size + 1;
+    int *dynArr = malloc(sizeHolder * sizeof(int));
+    *(dynArr)=size;
 
     //we should check that malloc was actually
     //successful(that we don't get a null pointer
 
     if(dynArr == NULL){
-    fprintf(stderr,"ERROR:  Array was not successfully created.\n");
-    return -1;
+        fprintf(stderr,"ERROR:  Array was not successfully created.\n");
+        return -1;
     }
 
-    //set the last entry to NULL
-    *(dynArr+size)=NULL;
-
     //return a pointer to newly allocated memory.
-
     return dynArr;
 
 }
@@ -37,7 +37,10 @@ int *createDynArr(int size){
 
 /*************************************************
 Function: findSize()
-Purpose:  Determines the size of the array.
+Purpose:  Determines the size of the dynamic array.
+
+        !!!!!  DO NOT USE WITH STATIC ARRAY !!!!
+
 Input:    A pointer to the array
 Output:   Returns the size of the array.
 
@@ -48,14 +51,10 @@ Other Changes: None
 
 int findSize(int *arr){
 
-    int count = 0;
-    while(*arr!=NULL){
-        arr++;
-        count++;
-    }
-    return count;
+  return *(arr);
 
 }
+
 
 /*************************************************
 Function: printSize()
@@ -70,8 +69,36 @@ Other Changes:  None
 
 void printSize(int *arr){
 
-    int size = findSize(arr);
-    printf("This is the size of your array: %d\n",size);
+    printf("This is the size of your array: %d\n",*(arr));
 
 }
 
+
+/*************************************************
+Function: appendArray()
+Purpose:  Appends an array to the end of the array.
+Input:    Pointers to the 1)array being appended
+                          2)array to be appended
+Output:   Returns 1 if successful, -1 if fail.
+
+Other Changes:  None
+
+*************************************************/
+
+void appendArray(int *arr, int *data){
+
+    //Kill the bottom and use realloc() instead!!!!!
+
+    int size = findSize(arr);
+    int *newArr;
+    newArr = arr+size;
+
+    //append the data
+//    for(int dataSize=findSizeArray(data)-1;
+//        dataSize>=0;dataSize--){
+
+//        *(newArr+dataSize) = data[dataSize];
+
+//        }
+
+}
--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 int main(){
 
-int *test = createDynArr(10);
-printSize(test);
+int *test = createDynArr(100);
+int *t3 = createDynArr(5000);
+printSize(t3);
+
 }
